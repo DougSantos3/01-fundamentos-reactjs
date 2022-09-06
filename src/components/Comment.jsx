d172ba1dcb5862f596a4fe9d2f0f879717dcbfab
DouglasMartins <douglas.santos@tinnova.com.br>
2022-09-06 08:58:26 -0300

    start remove comement part 1

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/douglasmartins7.png" alt=''/>
@@ -15,7 +19,7 @@ export function Comment({ content }) {
                             <time title="09 de Agosto ás 09:32:30" dateTime="2022-08-09 09:32:30">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24}/>
                         </button>
                     </header>
